ee97fbfcef0990c7eb2c63a962cb7e305883b951
Carlos-kaspa <carloslimatwg@gmail.com>
2021-10-24 16:39:30 -0300

    feat(db-migration): integração postgresql

--- src/services/UserProfileService.ts
+++ src/services/UserProfileService.ts
@@ -1,7 +1,7 @@
 import { prismaClient } from "../prisma";
 
 export class UserProfileService {
-    async execute(user_id: string) {
+    async execute(user_id: number) {
         return await prismaClient.user.findFirst({
             where: {
                 id: user_id
